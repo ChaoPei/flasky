bdbfc2d883d9dd814caade2b25fed0dd1f424ccf
Parlefan <1024088134@qq.com>
2016-10-04 16:45:44 +0800

    update email config

--- config.py
+++ config.py
@@ -11,9 +11,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'l love zhangfan'  # 私密配置从环境变量导入
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASK_MAIL_SENDER = 'peichao_hainu@163.com'
+    
+    # 邮件设置  
+    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'          # 邮件主题前缀
+    FLASK_MAIL_SENDER = 'peichao_hainu@163.com'     # 收件人
     FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_SERVER = 'peichao_pku@163.com'             # 发件人
+    MAIL_PORT = 25 #SSL 465
+    MAIL_USR_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     @staticmethod
     def init_app(app):
@@ -22,11 +29,6 @@ class Config:
 # 部署配置
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'peichao_pku@163.com'
-    MAIL_PORT = 25 #SSL 465
-    MAIL_USR_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
