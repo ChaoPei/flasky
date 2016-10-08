fceffe3d0ff4187cb7a2d5b2343baf6ffd1c618c
Parlefan <1024088134@qq.com>
2016-10-08 15:47:40 +0800

    permission required deco

---
+++ app/decorators.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+'''
+权限装饰器, 装饰路由函数, 检查当前用户权限是否能够访问路由函数的页面
+'''
+from functools import wraps
+from flask import abort
+from flask_login import current_user
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+
+def admin_required(f):
+    return permission_required(Permission.ADMINISTER)(f)
+
